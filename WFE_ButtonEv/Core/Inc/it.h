67e9b05fa516ba71f7629513f8170fe0e2c5f05b
NguyenTran260601 <tranvannguyen2606@gmail.com>
2023-03-21 20:17:43 +0700

    Low power mode processor
    Add WFI and WFE intructions.

---
+++ WFE_ButtonEv/Core/Inc/it.h
@@ -0,0 +1,13 @@
+/*
+ * it.h
+ *
+ *  Created on: Mar 20, 2023
+ *      Author: Nguyen Tran
+ */
+
+#ifndef INC_IT_H_
+#define INC_IT_H_
+
+
+
+#endif /* INC_IT_H_ */
---
+++ WFE_ButtonEv/Core/Src/data.c
@@ -0,0 +1,11 @@
+/*
+ * data.c
+ *
+ *  Created on: Mar 19, 2023
+ *      Author: Nguyen Tran
+ */
+
+#include "main.h"
+
+
+char somedata[] = "Testing WFE ISA\r\n";
